077088e301813a7040e77188d7a810f2e6219ebc
ANDUYONG <69151158+ANDUYONG@users.noreply.github.com>
2025-10-01 16:14:54 +0900

    ㅗㅗㅗ

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@ function onChangeItem(payload) {
           selectedFile.value = {
             ...response.data,
             decodeData: decodeBase64(response.data.content), 
-          }; // Decode base64 contentg
+          }; // Decode base64 contentgㅗ
         })
         .catch(error => {
           selectedFile.value = {};
