87116197598ea8c6c28ac34cbec0994ffc20e49a
ANDUYONG <69151158+ANDUYONG@users.noreply.github.com>
2025-10-12 16:10:38 +0900

    test1 커밋
    test1 커밋

--- src/http/git-hub-api.js
+++ src/http/git-hub-api.js
@@ -6,6 +6,8 @@ const GitHubAPI = {
     getTreeList: () => http.get(getApiPath('files')),
     getContent: ({ filePath, branch }) => http.get(getApiPath('file'), { params: { filePath, branch } }),
     commitContent: (data) => http.post(getApiPath('commitAndPush'), data),
+    branches: (branch) => http.post(getApiPath('branches')),
+    createBranch: (branch) => http.post(getApiPath('createBranch'), branch),
 }
 
 function getApiPath(path) {
